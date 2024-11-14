3d3a2ad137d30e310265e46e1787012b5581eb74
TanhBb1911 <tanh191202@gmail.com>
2024-11-14 17:12:06 +0700

    fix drop layout

--- src/App.tsx
+++ src/App.tsx
@@ -34,148 +34,142 @@ const App: React.FC = () => {
   useEffect(() => {
     console.log(activeId + " is dragging");
   }, [activeId]);
+  
+const FindToAdd = (id: string, detail: any, parent_id: string) => {
+  const newData = JSON.parse(JSON.stringify(data));
 
-  const FindToAdd = (id: string, detail: any, parent_id: string) => {
-    const newData = JSON.parse(JSON.stringify(data));
+  let layoutChilds: Obj[] = [];
 
-    const removeChildFromParent = (nodes: Obj[]) => {
-      nodes.forEach((node) => {
-        // Tìm và xóa item với `id` khỏi `node.childs`
-        node.childs = node.childs.filter((child) => child.id !== id);
+  const removeChildFromParent = (nodes: Obj[]) => {
+    nodes.forEach(node => {
+      const targetChild = node.childs.find(child => child.id === id);
+      if (targetChild) {
+        layoutChilds = targetChild.childs;
+      }
+      node.childs = node.childs.filter(child => child.id !== id);
 
-        // Đệ quy nếu `node.childs` có phần tử con
-        if (node.childs.length > 0) {
-          removeChildFromParent(node.childs);
-        }
-      });
-    };
-
-    // Xóa item khỏi layout gốc
-    removeChildFromParent([newData]);
-
-    const addChildToParent = (nodes: Obj[]) => {
-      nodes.forEach((node) => {
-        // Nếu `node.id` là `parent_id` và `node.childs` không chứa `id` thì thêm mới
-        if (
-          node.id === parent_id &&
-          !node.childs.some((child) => child.id === id)
-        ) {
-          node.childs = [
-            ...node.childs,
-            {
-              id,
-              columns: detail.columns,
-              rows: detail.rows,
-              colspan: detail.colspan,
-              rowspan: detail.rowspan,
-              type: detail.type,
-              childs: [],
-            },
-          ];
-        } else if (node.childs.length > 0) {
-          // Đệ quy vào `childs` nếu `node.childs` có phần tử
-          addChildToParent(node.childs);
-        }
-      });
-    };
-
-    if (newData.id === parent_id) {
-      newData.childs.push({
-        id,
-        columns: detail.columns,
-        rows: detail.rows,
-        colspan: detail.colspan,
-        rowspan: detail.rowspan,
-        type: detail.type,
-        childs: [],
-      });
-    } else {
-      addChildToParent(newData.childs);
-    }
-
-    dispatch(setData(newData));
-
-    setTimeout(() => {
-      console.log("Updated Data:", newData);
-    }, 1000);
+      if (node.childs.length > 0) {
+        removeChildFromParent(node.childs);
+      }
+    });
   };
 
-  const handleDragEnd = (event: DragEndEvent) => {
-    const {over, active} = event;
+  removeChildFromParent([newData]);
 
-    if (over && active.id !== over.id) {
-      FindToAdd(active.id.toString(), activeData, over.id.toString());
-
-      console.log("Sidebar before filter:", sidebar);
-      const updatedSidebar = sidebar.filter((sb) => sb.id !== active.id);
-      dispatch(setSidebar(updatedSidebar));
-      console.log("Sidebar after filter:", updatedSidebar);
-      // dispatch(setActiveId(null));
-      // dispatch(setActiveData(null));
-    }
+  const addChildToParent = (nodes: Obj[]) => {
+    nodes.forEach(node => {
+      if (
+        node.id === parent_id &&
+        !node.childs.some(child => child.id === id)
+      ) {
+        node.childs = [
+          ...node.childs,
+          {
+            id,
+            columns: detail.columns,
+            rows: detail.rows,
+            colspan: detail.colspan,
+            rowspan: detail.rowspan,
+            type: detail.type,
+            childs: layoutChilds,
+          },
+        ];
+      } else if (node.childs.length > 0) {
+        addChildToParent(node.childs);
+      }
+    });
   };
 
-  const handleDragMove = (event: DragMoveEvent) => {
-    // console.log(event);
-  };
+  if (newData.id === parent_id) {
+    newData.childs.push({
+      id,
+      columns: detail.columns,
+      rows: detail.rows,
+      colspan: detail.colspan,
+      rowspan: detail.rowspan,
+      type: detail.type,
+      childs: layoutChilds,
+    });
+  } else {
+    addChildToParent(newData.childs);
+  }
 
-  return (
-    <DndContext
-      collisionDetection={pointerWithin}
-      onDragStart={(e: DragEndEvent) => {
-        dispatch(setColumns(e.active.data.current.columns));
-        dispatch(setRows(e.active.data.current.rows));
-        dispatch(setColspan(e.active.data.current.colspan));
-        dispatch(setRowspan(e.active.data.current.rowspan));
+  dispatch(setData(newData));
 
-        dispatch(setActiveId(e.active.id));
-        dispatch(setActiveData(e.active.data.current));
-      }}
-      onDragEnd={handleDragEnd}
-      onDragMove={handleDragMove}
-    >
-      <div className="flex items-start w-full bg-violet-100">
-        <Sidebar />
-        <div className="bg-violet-100 w-full p-6 z-10">
-          <div className="bg-white mx-auto max-w-[75rem] w-full min-h-screen">
-            <Droppable
-              detail={{
-                columns: data.columns,
-                rows: data.rows,
-                colspan: data.colspan,
-                rowspan: data.rowspan,
-                type: data.type,
-              }}
+  setTimeout(() => {
+    console.log("Updated Data:", newData);
+  }, 1000);
+};
+
+const handleDragEnd = (event: DragEndEvent) => {
+  const { over, active } = event;
+
+  if (over && active.id !== over.id) {
+    FindToAdd(active.id.toString(), activeData, over.id.toString());
+
+    console.log("Sidebar before filter:", sidebar);
+    const updatedSidebar = sidebar.filter(sb => sb.id !== active.id);
+    dispatch(setSidebar(updatedSidebar));
+    console.log("Sidebar after filter:", updatedSidebar);
+  }
+};
+
+const handleDragMove = (event: DragMoveEvent) => {};
+
+return (
+  <DndContext
+    collisionDetection={pointerWithin}
+    onDragStart={(e: DragEndEvent) => {
+      dispatch(setColumns(e.active.data.current.columns));
+      dispatch(setRows(e.active.data.current.rows));
+      dispatch(setColspan(e.active.data.current.colspan));
+      dispatch(setRowspan(e.active.data.current.rowspan));
+
+      dispatch(setActiveId(e.active.id));
+      dispatch(setActiveData(e.active.data.current));
+    }}
+    onDragEnd={handleDragEnd}
+    onDragMove={handleDragMove}>
+    <div className="flex items-start w-full bg-violet-100">
+      <Sidebar />
+      <div className="bg-violet-100 w-full p-6 z-10">
+        <div className="bg-white mx-auto max-w-[75rem] w-full min-h-screen">
+          <Droppable
+            detail={{
+              columns: data.columns,
+              rows: data.rows,
+              colspan: data.colspan,
+              rowspan: data.rowspan,
+              type: data.type,
+            }}
+            id={data.id}>
+            <ItemsRenderer
+              childs={data.childs}
               id={data.id}
-            >
-              <ItemsRenderer
-                childs={data.childs}
-                id={data.id}
-                columns={data.columns}
-                rows={data.rows}
-                colspan={data.colspan}
-                rowspan={data.rowspan}
-                currentDepth={1}
-                type={data.type}
-              />
-            </Droppable>
-          </div>
+              columns={data.columns}
+              rows={data.rows}
+              colspan={data.colspan}
+              rowspan={data.rowspan}
+              currentDepth={1}
+              type={data.type}
+            />
+          </Droppable>
         </div>
-        <PropertiesBar />
       </div>
-      {/* <DragOverlay
-        style={{
-          zIndex: 9999,
-          pointerEvents: "none",
-          position: "fixed",
-        }}
-      >
-        {activeId ? (
-          <div className="bg-slate-50 w-full h-full" style={{zIndex: 9999}} />
-        ) : null}
-      </DragOverlay> */}
-    </DndContext>
-  );
+      <PropertiesBar />
+    </div>
+    <DragOverlay
+      style={{
+        zIndex: 9999,
+        pointerEvents: "none",
+        position: "fixed",
+      }}>
+      {activeId ? (
+        <div className="bg-slate-50 w-full h-full" style={{ zIndex: 9999 }} />
+      ) : null}
+    </DragOverlay>
+  </DndContext>
+);
 };
 
 export default App;
