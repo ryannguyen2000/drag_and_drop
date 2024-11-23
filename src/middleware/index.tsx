7dfdddc74370b83bac1b02357da924affd0d8e65
datnt19213 <datnguyen.frontend.dev@gmail.com>
2024-11-23 11:27:46 +0700

    chore: comment middlware

--- src/middleware/index.tsx
+++ src/middleware/index.tsx
@@ -7,22 +7,22 @@ const Middleware = () => {
   const navigate = useNavigate();
   const [allow, setAllow] = useState(false);
 
-//   useEffect(() => {
-//     const check = async () => {
-//       if (!GetACookie("dcid") && location.pathname === "/editor") {
-//         await setTimeout(() => {
-//           setAllow(false);
-//         }, 500);
-//         return;
-//       }
-//       setAllow(true);
-//     };
-//     check();
-//   }, [location, navigate]);
+  //   useEffect(() => {
+  //     const check = async () => {
+  //       if (!GetACookie("dcid") && location.pathname === "/editor") {
+  //         await setTimeout(() => {
+  //           setAllow(false);
+  //         }, 500);
+  //         return;
+  //       }
+  //       setAllow(true);
+  //     };
+  //     check();
+  //   }, [location, navigate]);
 
-//   if (!allow) {
-//     return <Navigate to="/" state={{from: location}} replace />;
-//   }
+  //   if (!allow) {
+  //     return <Navigate to="/" state={{from: location}} replace />;
+  //   }
 
   return <Outlet />;
 };
